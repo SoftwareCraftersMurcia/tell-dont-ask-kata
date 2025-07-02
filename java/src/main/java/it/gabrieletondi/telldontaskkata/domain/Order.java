832e751b6f4386a11d095af3a22b99080ccd3939
Alberto Morales <kodenix@gmail.com>
2025-07-02 23:01:01 +0200

    refactor: move order initialization logic to setDefaultValues method in Order class

--- java/src/main/java/it/gabrieletondi/telldontaskkata/domain/Order.java
+++ java/src/main/java/it/gabrieletondi/telldontaskkata/domain/Order.java
@@ -1,6 +1,7 @@
 package it.gabrieletondi.telldontaskkata.domain;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
@@ -12,6 +13,7 @@ public class Order {
     private int id;
 
     public Order() {
+        setDefaultValues(this);
     }
 
     public Order(BigDecimal total, String currency, List<OrderItem> items, BigDecimal tax, OrderStatus status, int id) {
@@ -21,6 +23,14 @@ public class Order {
         this.tax = tax;
         this.status = status;
         this.id = id;
+    }
+
+    public static void setDefaultValues(Order order) {
+        order.setStatus(OrderStatus.CREATED);
+        order.setItems(new ArrayList<>());
+        order.setCurrency("EUR");
+        order.setTotal(new BigDecimal("0.00"));
+        order.setTax(new BigDecimal("0.00"));
     }
 
     public void setTaxedAmount(BigDecimal taxedAmount) {
