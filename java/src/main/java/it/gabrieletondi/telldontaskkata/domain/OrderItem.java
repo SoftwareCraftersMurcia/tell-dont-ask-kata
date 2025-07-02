825946b8937ec67517da4d127ddaf17f19b5cb68
Alberto Morales <kodenix@gmail.com>
2025-07-02 23:31:30 +0200

    refactor: set values into Order Item constructor

--- java/src/main/java/it/gabrieletondi/telldontaskkata/domain/OrderItem.java
+++ java/src/main/java/it/gabrieletondi/telldontaskkata/domain/OrderItem.java
@@ -2,6 +2,9 @@ package it.gabrieletondi.telldontaskkata.domain;
 
 import java.math.BigDecimal;
 
+import static java.math.BigDecimal.valueOf;
+import static java.math.RoundingMode.HALF_UP;
+
 public class OrderItem {
     private Product product;
     private int quantity;
@@ -9,10 +12,14 @@ public class OrderItem {
     private BigDecimal tax;
 
     public OrderItem(Product product, int quantity, BigDecimal taxAmount, BigDecimal taxedAmount) {
+        BigDecimal productPrice = product.getPrice();
+        final BigDecimal quantityTemp = BigDecimal.valueOf(quantity);
+        final BigDecimal taxedAmountTemp = productPrice.add(productPrice.divide(valueOf(100)).multiply(product.getCategory().getTaxPercentage()).setScale(2, HALF_UP)).setScale(2, HALF_UP).multiply(quantityTemp).setScale(2, HALF_UP);
+        final BigDecimal taxAmountTemp = productPrice.divide(valueOf(100)).multiply(product.getCategory().getTaxPercentage()).setScale(2, HALF_UP).multiply(quantityTemp);
         this.setProduct(product);
         this.setQuantity(quantity);
-        this.setTax(taxAmount);
-        this.setTaxedAmount(taxedAmount);
+        this.setTax(taxAmountTemp);
+        this.setTaxedAmount(taxedAmountTemp);
     }
 
     public Product getProduct() {
