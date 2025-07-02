fea7dc8c6a0bf1afbf72a0fbba33ca7aa2c58d8b
Alberto Morales <kodenix@gmail.com>
2025-07-02 22:42:21 +0200

    refactor: extract order initialization to setInitValues method in OrderCreationUseCase

--- java/src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCase.java
+++ java/src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCase.java
@@ -24,11 +24,7 @@ public class OrderCreationUseCase {
 
     public void run(SellItemsRequest request) {
         Order order = new Order();
-        order.setStatus(OrderStatus.CREATED);
-        order.setItems(new ArrayList<>());
-        order.setCurrency("EUR");
-        order.setTotal(new BigDecimal("0.00"));
-        order.setTax(new BigDecimal("0.00"));
+        setInitValues(order);
 
         for (SellItemRequest itemRequest : request.getRequests()) {
             Product product = productCatalog.getByName(itemRequest.getProductName());
@@ -55,6 +51,14 @@ public class OrderCreationUseCase {
         }
 
         orderRepository.save(order);
+    }
+
+    private static void setInitValues(Order order) {
+        order.setStatus(OrderStatus.CREATED);
+        order.setItems(new ArrayList<>());
+        order.setCurrency("EUR");
+        order.setTotal(new BigDecimal("0.00"));
+        order.setTax(new BigDecimal("0.00"));
     }
 
 }
