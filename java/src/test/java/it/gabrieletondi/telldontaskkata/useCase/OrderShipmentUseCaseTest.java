f9102644a1d561803ebca07e1e439ef9b559000c
Alberto Morales <kodenix@gmail.com>
2025-07-03 22:49:00 +0200

    refactor: streamline order creation in shipment tests

--- java/src/test/java/it/gabrieletondi/telldontaskkata/useCase/OrderShipmentUseCaseTest.java
+++ java/src/test/java/it/gabrieletondi/telldontaskkata/useCase/OrderShipmentUseCaseTest.java
@@ -17,16 +17,14 @@ public class OrderShipmentUseCaseTest {
 
     @Test
     public void shipApprovedOrder() {
-        Order initialOrder = Order.createApprovedWithId(1);
-        orderRepository.addOrder(initialOrder);
-
+        orderRepository.addOrder(Order.createApprovedWithId(1));
         OrderShipmentRequest request = new OrderShipmentRequest();
         request.setOrderId(1);
 
         useCase.run(request);
 
         assertThat(orderRepository.getSavedOrder().getStatus()).isEqualTo(OrderStatus.SHIPPED);
-        assertThat(shipmentService.getShippedOrder()).isEqualTo(initialOrder);
+        assertThat(shipmentService.getShippedOrder()).isEqualTo(Order.createApprovedWithId(1));
     }
 
     @Test
@@ -44,7 +42,6 @@ public class OrderShipmentUseCaseTest {
     @Test
     public void rejectedOrdersCannotBeShipped() {
         orderRepository.addOrder(Order.createRejectedWithId(1));
-
         OrderShipmentRequest request = new OrderShipmentRequest();
         request.setOrderId(1);
 
@@ -55,9 +52,7 @@ public class OrderShipmentUseCaseTest {
 
     @Test
     public void shippedOrdersCannotBeShippedAgain() {
-        Order initialOrder = Order.createShippedWithId(1);
-        orderRepository.addOrder(initialOrder);
-
+        orderRepository.addOrder(Order.createShippedWithId(1));
         OrderShipmentRequest request = new OrderShipmentRequest();
         request.setOrderId(1);
 
