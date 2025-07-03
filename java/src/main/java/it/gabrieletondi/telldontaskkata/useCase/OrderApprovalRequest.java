00b1e2476b22f826141f0a3009acaf20d53fa7d2
Alberto Morales <kodenix@gmail.com>
2025-07-03 23:04:25 +0200

    refactor: remove unused constructors and setters in OrderApprovalRequest

--- java/src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderApprovalRequest.java
+++ java/src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderApprovalRequest.java
@@ -4,24 +4,13 @@ public class OrderApprovalRequest {
     private int orderId;
     private boolean approved;
 
-    public OrderApprovalRequest() {
-    }
-
     public OrderApprovalRequest(int orderId, boolean approved) {
         this.orderId = orderId;
         this.approved = approved;
     }
 
-    public void setOrderId(int orderId) {
-        this.orderId = orderId;
-    }
-
     public int getOrderId() {
         return orderId;
-    }
-
-    public void setApproved(boolean approved) {
-        this.approved = approved;
     }
 
     public boolean isApproved() {
