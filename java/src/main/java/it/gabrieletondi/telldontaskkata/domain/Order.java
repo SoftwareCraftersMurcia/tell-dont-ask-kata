2634a99632b0b8fd6575d7ed1e4334c8536045df
Alberto Morales <kodenix@gmail.com>
2025-07-04 15:56:49 +0200

    refactor: change setStatus method to private and remove public setter for status field

--- java/src/main/java/it/gabrieletondi/telldontaskkata/domain/Order.java
+++ java/src/main/java/it/gabrieletondi/telldontaskkata/domain/Order.java
@@ -111,10 +111,6 @@ public class Order {
         return status;
     }
 
-    public void setStatus(OrderStatus status) {
-        this.status = status;
-    }
-
     public int getId() {
         return id;
     }
@@ -128,6 +124,10 @@ public class Order {
                 getCurrency(),
                 getItems()
         );
+    }
+
+    private void setStatus(OrderStatus status) {
+        this.status = status;
     }
 
     private boolean isCreated() {
